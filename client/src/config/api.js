ea4538ea7e084a8dc18d2cbf893a91f977e80612
KostasBzn <kostasbouzianis@gmail.com>
2024-02-05 15:47:38 +0100

    commit

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,1 +1,4 @@
-export const baseURL = "http://localhost:3002";
+export const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://full-stack-to-do-app-react.vercel.app"
+    : "http://localhost:3002";
--- server/src/server.js
+++ server/src/server.js
@@ -7,7 +7,11 @@ import usersRoutes from "./routes/usersRoutes.js";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 connectDB();
 
